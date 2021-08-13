92002bbde440e6ea61861a0120a1b1119c608b03
intello-carlos <carlos.tovar@intelloidea.com>
2021-08-13 11:14:58 -0500

    [ADD] - Creacion modelo, para agregar campo tipo de cliente

--- solution-pharmacy/models/__init__.py
+++ solution-pharmacy/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import models
 from . import products_quatator
+from . import res_partner
--- solution-pharmacy/models/res_partner.py
+++ solution-pharmacy/models/res_partner.py
@@ -2,11 +2,12 @@
 
 from odoo import models, fields, api
 
-class Solution(models.Model):
-    _name = "solution.pharmacy.quatator"
-    _description = "Solution Pharmacy Quatator"
+class RestPartnerType(models.Model):
+    _inherit = "res.partner"
+    _description = "Add customer type field"
 
-    partner_id = fields.Many2one('res.partner', string='Client', required=True)
     type_client = fields.Selection(String="Type client", 
                                    selection=[('specialist', 'Specialist'), ('distributor', 'Distributor'), ('patient', 'Patient')],
-                                   required=True)
+                                   requeried=True,
+                                   store=True)
+    
