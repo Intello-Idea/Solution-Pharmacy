a76af387e652bfd4295097a2fa10dd1c3319a277
intello-carlos <carlos.tovar@intelloidea.com>
2021-08-13 16:45:43 -0500

    [ADD] - Creacion de campos formula individual

--- solution-pharmacy/models/products_quatator.py
+++ solution-pharmacy/models/products_quatator.py
@@ -5,8 +5,16 @@ from odoo import models, fields, api
 class Solution(models.Model):
     _name = "solution.pharmacy.quatator"
     _description = "Solution Pharmacy Quatator"
+    _rec_name = "partner_id"
 
     partner_id = fields.Many2one('res.partner', string='Client', required=True)
-    type_client = fields.Selection(String="Type client", 
-                                   selection=[('specialist', 'Specialist'), ('distributor', 'Distributor'), ('patient', 'Patient')],
-                                   required=True)
+    user = fields.Many2one('res.users', string='User', required=True)
+    size = fields.Integer(string="Size", required=True)
+    pharmaceutical_form = fields.Integer(string="Farmaceutical form", required=True)
+    date = fields.Date(string="Date quatator", readonly=True, index=True, default=fields.Date.context_today)
+    term = fields.Text()
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('posted', 'Open'),
+        ('cancel', 'Cancelled')
+        ], string='Invoice Status', readonly=True)
