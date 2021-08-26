ee960c7d40429469b0ebe542e1bb4e48de0837c8
intello-carlos <carlos.tovar@intelloidea.com>
2021-08-26 17:05:46 -0500

    [ADD] Avances del cotizador

--- solution-pharmacy/models/products_quotator.py
+++ solution-pharmacy/models/products_quotator.py
@@ -5,21 +5,22 @@ from odoo import models, fields, api
 class Solution(models.Model):
     _name = "solution.pharmacy.quotator"
     _description = "Solution Pharmacy Quotator"
-    _rec_name = "partner_id"
-
+    
+    name = fields.Char(string="Name quotator")
     partner_id = fields.Many2one('res.partner', string='Client', required=True)
     user = fields.Many2one('res.users', string='Quotator', required=True, readonly=True, default=lambda self: self.env.user)
     quotator_date = fields.Date(string="Quotator date", readonly=True, index=True, default=fields.Date.context_today)
     expiration_date = fields.Date(string="Expiration")
-    pharmaceutical_form = fields.Many2one('product.product', string="Pharmaceutical form")
     pricelist_id = fields.Many2one('product.pricelist', string="Pricelist", related="partner_id.property_product_pricelist", readonly=True)
     appointment_lines = fields.One2many('solution.pharmacy.quotator.lines', 'appointment_id', string="Material")
-    final_product = fields.One2many('product.lines', 'product_lines', string="Final product")
+    final_product = fields.One2many('product.lines', 'final_product_lines', string="Final product")
     state = fields.Selection([
         ('draft', 'Draft'),
         ('posted', 'Open'),
         ('cancel', 'Cancelled')
         ], string='Invoice Status', readonly=True)
-        
 
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', '!!! La referencia (Name quotator) ya existe por favor cambiela !!!')
+    ]
 
--- solution-pharmacy/models/products_quotator_lines.py
+++ solution-pharmacy/models/products_quotator_lines.py
@@ -5,30 +5,33 @@ from odoo import models, fields, api
 class SolutionLines(models.Model):
     _name = "solution.pharmacy.quotator.lines"
     _description = "Solution Pharmacy Quotator Lines"
+    _rec_name = "product_id"
 
 
     product_id = fields.Many2one('product.product', string="Product", required=True)
     product_qty = fields.Float(string="Quantity", default=1.0, digits='Product Unit of Measure', required=True)
     percentage = fields.Float(string="Percentage(%)", required=True, default=0)
-    price_unit = fields.Float('Unit Price', required=True, default=0.0, compute="_update_price")
-    price_subtotal = fields.Float('Subtotal price', compute="_compute_price_subtotal")
-    appointment_id = fields.Many2one('solution.pharmacy.quotator', string="Appointment id")
+    price_unit = fields.Float('Unit Price', required=True, default=0.0, compute="_update_price", store=True)
+    price_subtotal = fields.Float('Subtotal price', compute="_compute_price_subtotal", store=True)
+    appointment_id = fields.Many2one('solution.pharmacy.quotator', string="Appointment id", store=True)
+    final_product_id = fields.Many2one('product.lines', string="Final product", domain="[('final_product_lines', '=', appointment_id)]") 
     tax_id = fields.Many2one(
         'account.tax',
         domain="[('type_tax_use','=','sale'), ('company_id', '=', company_id)]", check_company=True)
 
     @api.depends('product_id')
     def _update_price(self):
-        if self.product_id:
-            price = self.appointment_id.pricelist_id.get_product_price(self.product_id, 1.0 or self.product_qty, self.appointment_id.partner_id)
-            if price is False:
-                warning = {
-                    'title': _('No valid pricelist line found.'),
-                    'message':
-                        _("Couldn't find a pricelist line matching this product and quantity.\nYou have to change either the product, the quantity or the pricelist.")}
-                return {'warning': warning}
-            else:
-                self.price_unit = price
+        for line in self:
+            if line.product_id:
+                price = line.appointment_id.pricelist_id.get_product_price(line.product_id, 1.0 or line.product_qty, line.appointment_id.partner_id)
+                if price is False:
+                    warning = {
+                        'title': _('No valid pricelist line found.'),
+                        'message':
+                            _("Couldn't find a pricelist line matching this product and quantity.\nYou have to change either the product, the quantity or the pricelist.")}
+                    return {'warning': warning}
+                else:
+                    line.price_unit = price
     
     @api.depends('price_unit', 'appointment_id', 'product_qty', 'product_id')
     def _compute_price_subtotal(self):
@@ -36,23 +39,33 @@ class SolutionLines(models.Model):
                taxes = line.tax_id.compute_all(line.price_unit, line.appointment_id.pricelist_id.currency_id, line.product_qty, line.product_id, line.appointment_id.partner_id)
                line.price_subtotal = taxes['total_excluded']
     
-    @api.onchange('percentage')
+    @api.onchange('final_product_id', 'percentage')
     def _compute_qty(self):
         for line in self:
-            line.product_qty = line.percentage
-
+            line.product_qty = (line.final_product_id.size_subtotal*line.percentage)/100
 
 class Productlines(models.Model):
     _name = "product.lines"
     _description = "Este modelo crea lineas de productos terminados"
+    _rec_name = "product_id"
 
-    product_id = fields.Many2one('product.product', string="Final product")
-    qty = fields.Integer(string="Quantity")
-    product_lines = fields.Many2one('solution.pharmacy.quotator', string="Final product")
-    size_subtotal = fields.Integer(string="subtotal size(gr)")
-    size_total = fields.Integer(string="Total size(gr)")
+    product_id = fields.Many2one('product.product', string="Final product", required=True)
+    qty = fields.Integer(string="Quantity", default=1.0)
+    final_product_lines = fields.Many2one('solution.pharmacy.quotator', string="Final product")
+    size_subtotal = fields.Integer(string="subtotal size(gr)", required=True)
+    size_total = fields.Integer(string="Total size(gr)", compute="_compute_size_total")
+    pharmaceutical_form = fields.Many2one('product.product', string="Pharmaceutical form", required=True)
+    value_pharmaceutical_form = fields.Float(string="Value form pharmaceutical")
 
-    @api.onchange('size_subtotal', 'qty')
+    @api.onchange('product_id')
+    def product(self):
+        products = []
+        for line in self:
+            products.append(line.product_id.id)
+        return print(products)
+
+
+    @api.depends('size_subtotal', 'qty')
     def _compute_size_total(self):
         for line in self:
             line.size_total = line.qty * line.size_subtotal
