a356082950878ca0e282b9101553af6c65c1636a
intello-carlos <carlos.tovar@intelloidea.com>
2021-12-07 12:47:59 -0500

    [FIX]-Modificaciones cotizador

--- base_solution/models/pharmaceutical_form.py
+++ base_solution/models/pharmaceutical_form.py
@@ -1,16 +1,6 @@
 from odoo import models, fields, api
 
 class PharmaceuticalForm(models.Model):
-    _name = "pharmaceutical.form"
-    _description = "Model that stores the dosage form"
+    _inherit = "pharmaceutical.form"
 
-    name = fields.Char(string="Compoung")
-    code = fields.Integer()
     value = fields.Float(string='Value x gr')
-
-    @api.model
-    def default_get(self, fields):
-        res = super(PharmaceuticalForm, self).default_get(fields)
-        # Almacena el codigo por defecto al abrir la interfaz del modelo
-        res.update({'code': len(self.env["pharmaceutical.form"].search([])) + 1})
-        return res
