a870d9041bf5544051e600b80ee0f8d1040e3bf5
intello-carlos <carlos.tovar@intelloidea.com>
2021-12-28 20:59:30 -0500

    [FIX]-Se completa el modulo de bonificados

--- bonification/models/sale_order.py
+++ bonification/models/sale_order.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError, UserError
+import math
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -18,6 +19,7 @@ class SaleOrder(models.Model):
             values = (line.start, line.final, line.quantity_product)
             vls.append(values)
         value_min = min(vls)
+        value_max = max(vls)
         '''Bucle para descartar las lineas que no cumplan el valor minimo que contemplan productos bonificados'''
         result = []
         for record in self.order_line:
@@ -30,7 +32,7 @@ class SaleOrder(models.Model):
         values = []
         for line in result:
             for record in vls:
-                if line[3] in range(record[0], record[1]+1):
+                if line[3] in range(record[0], record[1]+1) and line[3] < (value_min[0]+value_max[0]):
                     items = {
                             'product_id': line[0],
                             'name': line[1],
@@ -40,13 +42,35 @@ class SaleOrder(models.Model):
                             'bonus': True,
                         }
                     bonification.append((0,0,items))
+                ''''Se crea este bucle para agregar funcionalidad de cuando pasen el rango maximo
+                si para proximas ocaciones se requiere solo dejar la funcionalidad de los rangos, solo es quitar
+                esta parte del codigo y el en if anterior remover and line[3] < (value_min[0]+value_max[0])'''
+                if line[3] in range(record[0], record[1]+1) and line[3] >= (value_min[0]+value_max[0]):
+                    x_round = line[3]/value_max[0]
+                    x_round = math.floor(x_round)
+                    print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS:", x_round)
+                    x_residuo = line[3] % value_max[0]
+                    print("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE:", x_residuo)
+                    residuo = 0
+                    for x in vls:
+                        if x_residuo in range(x[0], x[1]+1):
+                            residuo = x[2]
+                    item2 = {
+                            'product_id': line[0],
+                            'name': line[1],
+                            'product_counterpart': line[2],
+                            'product_uom_qty': (record[2]*x_round)+residuo,
+                            'price_unit': 0,
+                            'bonus': True,
+                        }
+                    bonification.append((0,0,item2))
         vals = {
                 'order_line': bonification,
         }
         record_ids = self.env['sale.order'].search([('name', '=', self.name)])
         for record in record_ids:
             record.update(vals)
-#
+
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
