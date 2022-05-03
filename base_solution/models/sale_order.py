c7f0c659e7314a9e4096e0ebdbcc9c68da3cbf23
FabianVC <fabianvera728@gmail.com>
2022-05-03 10:33:34 -0500

    [fix] Solución error al confirmar cotización

--- base_solution/models/sale_order.py
+++ base_solution/models/sale_order.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-#Se actualiza la funcionalidad del boton confirmar, para que me genere una lista de materiales para un producto generico, dependiendo la cantidad de materias primas
+    #Se actualiza la funcionalidad del boton confirmar, para que me genere una lista de materiales para un producto generico, dependiendo la cantidad de materias primas
     form_pharmaceutical = fields.Many2one('pharmaceutical.form', string="Form pharmaceutical")
     pharmaceutical_presentation = fields.Many2one('pharmaceutical.presentation', string="Presentation pharmaceutical")
     grams_pharmaceutical = fields.Float(string='grams')
@@ -13,30 +13,33 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         raw = []
         values = []
-        if self.order_line.product_id.name == 'Generico cotizador':
-            for line in self.raw_material:
-                dic = {
-                    'product_id': line.product_id.id,
-                    'product_qty': line.product_qty,
-                    'percent': line.percentage,
+        index = 0
+        for prod in self.order_line.product_id:
+            if prod.name == 'Generico cotizador':
+                for line in self.raw_material:
+                    dic = {
+                        'product_id': line.product_id.id,
+                        'product_qty': line.product_qty,
+                        'percent': line.percentage,
+                    }
+                    raw.append((0,0,dic))
+                values = {
+                    'pharmaceutical_form': self.form_pharmaceutical.id,
+                    'pharmaceutical_presentation': self.pharmaceutical_presentation.id,
+                    'size': self.grams_pharmaceutical,
+                    'production_line_id': self.production_line_id.id,
+                    'product_tmpl_id': self.order_line.product_id[index].product_tmpl_id.id,
+                    'bom_line_ids': raw,
                 }
-                raw.append((0,0,dic))
-            values = {
-                'pharmaceutical_form': self.form_pharmaceutical.id,
-                'pharmaceutical_presentation': self.pharmaceutical_presentation.id,
-                'size': self.grams_pharmaceutical,
-                'production_line_id': self.production_line_id.id,
-                'product_tmpl_id': self.order_line.product_id.product_tmpl_id.id,
-                'bom_line_ids': raw,
-            }
-            record_ids = self.env['mrp.bom'].search([('product_tmpl_id', '=', self.order_line.product_id.product_tmpl_id.id)])
+                record_ids = self.env['mrp.bom'].search([('product_tmpl_id', '=', self.order_line.product_id[index].product_tmpl_id.id)])
 
-            record = self.env['mrp.bom'].search([('product_tmpl_id', '=', self.order_line.product_id.product_tmpl_id.id)]).bom_line_ids.bom_id
-            delete = self.env['mrp.bom.line'].search([('bom_id', '=', record._origin.id)])
-            delete.unlink()
-            for record in record_ids:
-                record.write(values)
-            res = super(SaleOrder, self).action_confirm()
-        else:
-            res = super(SaleOrder, self).action_confirm()
+                record = self.env['mrp.bom'].search([('product_tmpl_id', '=', self.order_line[index].product_id.product_tmpl_id.id)]).bom_line_ids.bom_id
+                delete = self.env['mrp.bom.line'].search([('bom_id', '=', record._origin.id)])
+                delete.unlink()
+                for record in record_ids:
+                    record.write(values)
+                res = super(SaleOrder, self).action_confirm()
+            else:
+                res = super(SaleOrder, self).action_confirm()
+            index += 1
         return res
