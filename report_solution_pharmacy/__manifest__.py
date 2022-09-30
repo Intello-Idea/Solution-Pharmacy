199aaccf15ee34af4fb2530d2b39fbc84992d5f7
rousemilano <routhberism@gmail.com>
2022-09-30 12:15:05 -0400

    [FIX-ADD]Subir cambios en el cotizador, base_epithelium y agregar modulo nuevo

---
+++ report_solution_pharmacy/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Report Invoice of Solution Pharmacy",
+    'description': 'Create report of invoice for solution pharmacy',
+    'author': "Intello Idea",
+    'website': "http://www.intelloidea.com",
+    'category': 'Accounting',
+    'version': '1.0',
+    'depends': ['base','web'],
+    'application': False,
+    'data': [
+
+        'report/template_invoice.xml',
+'report/main_report.xml',
+    ],
+}
