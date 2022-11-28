c0cedd33371608382aa9bdaea098b3dd61175af1
rousemilano <routhberism@gmail.com>
2022-11-28 10:51:14 -0400

    [FIX] Agregar modificar qwebs de facturas y vale de entrega

---
+++ custom_stock_report/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Stock Report",
+
+    'summary': """Personalizar reporte del módulo stock""",
+
+    'author': "Intello Idea",
+    'website': "http://www.intelloidea.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'stock'],
+
+    'data': [
+        'report/report_deliveryslip.xml',
+    ],
+}
