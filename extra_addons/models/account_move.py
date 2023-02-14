29de938af315b578a58207bc963ffccc5ae68b2b
CarlosAndresTovar <carlos.andres.tovarr@gmail.com>
2023-02-14 10:12:21 -0500

    ajuste para poder subir registros medico referido de forma masiva

--- extra_addons/models/account_move.py
+++ extra_addons/models/account_move.py
@@ -5,4 +5,4 @@ from odoo import models, api, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    referred_doctor = fields.Many2one('res.partner', string='Referred Doctor', related='partner_id.referred_doctor', store=True)
+    referred_doctor = fields.Many2one('res.partner', string='Referred Doctor', related='partner_id.referred_doctor', store=True, readonly=False)
--- extra_addons/models/sale_order.py
+++ extra_addons/models/sale_order.py
@@ -5,4 +5,4 @@ from odoo import models, api, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    referred_doctor = fields.Many2one('res.partner', string='Referred Doctor', related='partner_id.referred_doctor', store=True)
+    referred_doctor = fields.Many2one('res.partner', string='Referred Doctor', related='partner_id.referred_doctor', store=True, readonly=False)
