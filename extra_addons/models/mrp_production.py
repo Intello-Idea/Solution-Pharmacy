79593e6630209c0970a732652f7e156a7f9ebc6f
CarlosAndresTovar <carlos.andres.tovarr@gmail.com>
2023-04-20 20:50:32 -0500

    Agregar homologos en ordenes de producción y en qweb de las mismas

--- extra_addons/models/mrp_production.py
+++ extra_addons/models/mrp_production.py
@@ -7,3 +7,5 @@ class MrpBom(models.Model):
     _inherit = 'mrp.production'
 
     instruction_code = fields.Char(string="Instruction code", related='bom_id.instruction_code', store=True)
+    homologos = fields.Many2one('product.counterpart', string="Homologos")
+    tm_id = fields.Many2one(related='product_id.product_tmpl_id')
