5f578de9d4f9296ebef5e3922a2919a6e3a4342f
CarlosAndresTovar <carlos.andres.tovarr@gmail.com>
2023-05-03 08:56:31 -0500

    [44] traer vendedor asociado al cliente

--- extra_addons/models/account_move.py
+++ extra_addons/models/account_move.py
@@ -6,3 +6,5 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     referred_doctor = fields.Many2one('res.partner', string='Referred Doctor', related='partner_id.referred_doctor', store=True, readonly=False)
+    team_id = fields.Many2one(
+        'crm.team', string='Sales Team', related='partner_id.team_id')
